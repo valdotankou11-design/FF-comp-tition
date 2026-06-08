3eaf991481398275ed5eb4573ea0a875baa64b51
valdotankou11-design <valdotankou11@gmail.com>
2026-06-08 06:06:30 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -308,3 +308,8 @@ def get_stats():
 @app.route('/api/health')
 def health():
     return jsonify({'status':'ok','version':'2.0'})
+# pour Vercel
+application = app
+
+if __name__ == '__main__':
+    app.run(debug=True)
